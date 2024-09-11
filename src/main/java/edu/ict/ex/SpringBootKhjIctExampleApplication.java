bceb54a23df18b0c4a75c5e378ff565c4b95a4ad
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-09-11 11:34:47 +0900

    첫번째 커밋 스프링 기본 설정

---
+++ src/main/java/edu/ict/ex/SpringBootKhjIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKhjIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKhjIctExampleApplication.class, args);
+	}
+
+}
