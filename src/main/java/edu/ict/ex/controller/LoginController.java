97b59fdb5a711d52f20337c65b64445f677cfb0f
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-09-30 10:03:45 +0900

    서버에서 세션을 가져오는 3가지 방법

--- src/main/java/edu/ict/ex/controller/LoginController.java
+++ src/main/java/edu/ict/ex/controller/LoginController.java
@@ -1,5 +1,10 @@
 package edu.ict.ex.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +28,29 @@ public class LoginController {
 		return "/login/login";
 	}
 	
-	
+	@GetMapping("/loginInfo")
+	public String loginInfo(Authentication authentication , Principal principal, Model model) {
+		log.info("loginInfo()...");
+		
+		if(principal != null) {
+			System.out.println(principal);
+			System.out.println(principal.getName());
+			System.out.println(principal);
+		}
+		
+		if(authentication != null) {
+			System.out.println(authentication);
+			System.out.println(authentication.getName());
+			System.out.println(authentication.getAuthorities());
+		}
+		
+		// 정보를 수정 후 변경된 회원정보를 업데이트 하기 위해서 사용하는 방법
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		System.out.println(auth.getName());
+		System.out.println(auth.getAuthorities());
+		
+		return "redirect:/";
+	}
 	
 }
